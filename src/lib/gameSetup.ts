c7320d52a15c87c3a46d6814f8ac19f1daaaf1ec
Rickcern44 <cerny.ricky@yahoo.com>
2022-11-28 07:17:04 -0500

    Formatted arrays in the game setup class.

--- src/lib/gameSetup.ts
+++ src/lib/gameSetup.ts
@@ -16,6 +16,18 @@ export class Game {
 }
 
 function formatProblem(item: any){
-    return item;
+    let arr = []
+    console.log(`Game Setup : Array Length - ${item.length}`)
+    arr.push(Array(1).fill(Array.from(item.substring(0,9))));
+    arr.push(Array(1).fill(Array.from(item.substring(9,18))));
+    arr.push(Array(1).fill(Array.from(item.substring(18,27))));
+    arr.push(Array(1).fill(Array.from(item.substring(27,36))));
+    arr.push(Array(1).fill(Array.from(item.substring(36,45))));
+    arr.push(Array(1).fill(Array.from(item.substring(45,54))));
+    arr.push(Array(1).fill(Array.from(item.substring(54,63))));
+    arr.push(Array(1).fill(Array.from(item.substring(63,72))));
+    arr.push(Array(1).fill(Array.from(item.substring(72,81))));
+
+    return arr;
 }
 
