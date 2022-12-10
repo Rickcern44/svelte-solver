227f038bc9ae55aaeee413f005731bca256645ff
Rickcern44 <cerny.ricky@yahoo.com>
2022-12-10 10:40:52 -0500

    Removed the logo import

--- src/routes/game/aboutgame/+page.svelte
+++ src/routes/game/aboutgame/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-    import { error, redirect } from '@sveltejs/kit';
-    import logo from '$lib/images/SvelteLogo.jpg'
     /** @type {import('./$types').PageServerLoad} */
     export let data;
 
